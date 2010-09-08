fc6b7ecbf6ae78b7049f960d054c440c03a2cae9
Anthony Lieuallen <arantius@gmail.com>
2010-09-08 17:07:15 -0400

    Remove unused import.

--- util.py
+++ util.py
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import os
-import urllib2
 
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
