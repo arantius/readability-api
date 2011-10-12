abf3418f8e424bf1db74dc8c2185d4b40fdd6dca
Anthony Lieuallen <arantius@gmail.com>
2011-10-11 23:17:05 -0400

    exploratory word counting, frequent word dropping

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import re
 import urllib2
 
 from django.core.cache import cache
@@ -12,3 +13,23 @@ def getUrl(url):
 
   cache.set(cache_key, content)
   return content
+
+def words(s):
+  """Turn camel case and underscore/hyphen separated strings to lists of words.
+
+  e.g.
+  fooBarBaz -> ['foo', 'bar', 'baz]
+  foo_bar_baz -> ['foo', 'bar', 'baz]
+  foo-bar-baz -> ['foo', 'bar', 'baz]
+
+  Args:
+    s: Any string.
+
+  Returns:
+    List of Strings, as described.
+  """
+  s = re.sub('(.)([A-Z][a-z]+)', r'\1 \2', s)
+  s = re.sub('([a-z0-9])([A-Z])', r'\1 \2', s)
+  s = re.sub('[-_\s]+', ' ', s)
+  if not s: return []
+  return s.lower().strip().split(' ')
--- views.py
+++ views.py
@@ -2,6 +2,7 @@ import os
 
 from django import http
 from django import shortcuts
+from lxml import html
 
 import util
 
@@ -11,7 +12,35 @@ ROBOTS_PATH = os.path.join(
     os.path.dirname(__file__), 'static', 'robots.txt')
 
 def cleanPage(request):
-  return http.HttpResponse(util.getUrl(request.GET['url']))
+  url = request.GET['url']
+  content = util.getUrl(url)
+  doc = html.fromstring(content)
+
+  # Always unconditionally remove these elements.
+  for el in doc.xpath('//head | //script | //style'):
+    el.drop_tree()
+
+  # Build "classid" values, counting occurrances as we go.
+  word_freq = {}
+  for el in doc.xpath('//*[@class or @id]'):
+    words = set(
+        util.words(el.attrib.get('class', '')) +
+        util.words(el.attrib.get('id', ''))
+        )
+    for word in words:
+      word_freq.setdefault(word, 0)
+      word_freq[word] += 1
+    el.attrib['classid'] = ' '.join(words).strip()
+
+  for x in sorted([(v, k) for (k, v) in word_freq.items()]):
+    print x
+
+  # Add a base href.
+  base = html.Element('base', href=url)
+  doc.insert(0, base)
+
+  # Turn it back into HTML!
+  return http.HttpResponse(html.tostring(doc, encoding=unicode))
 
 def favicon(request):
   return http.HttpResponse(
