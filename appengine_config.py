d097c75a5aea060aabff08d432c84f453f1f1b9a
Anthony Lieuallen <arantius@gmail.com>
2014-08-26 17:17:37 -0400

    disable appstats

--- appengine_config.py
+++ appengine_config.py
@@ -1,7 +1,7 @@
 import os
 
-if 'Development' not in os.environ.get('SERVER_SOFTWARE', ''):
-  def webapp_add_wsgi_middleware(app):
-    from google.appengine.ext.appstats import recording
-    app = recording.appstats_wsgi_middleware(app)
-    return app
+#if 'Development' not in os.environ.get('SERVER_SOFTWARE', ''):
+#  def webapp_add_wsgi_middleware(app):
+#    from google.appengine.ext.appstats import recording
+#    app = recording.appstats_wsgi_middleware(app)
+#    return app
