8ccb54cb11853a3cb1a5987465fad646ce3118ba
Anthony Lieuallen <arantius@gmail.com>
2021-03-29 20:54:27 -0400

    Define an ALLOWED_HOSTS for running without DEBUG.
    This might not be the right thing, but is good enough for now.  Still developing locally only.

--- readability/settings.py
+++ readability/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 DATABASES = {
   'default': {
